f6e2d3e8a283273aebec04fad397cefced2bd757
Chris Bouwhuis <chrisb190203@gmail.com>
2024-05-30 16:10:53 +0200

    ik moet dit mogelijk nog gaan op splitsen, maar ik heb het nu op deze manier gedaan aangezien ik dit mooier vind

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -2,23 +2,8 @@
     :pageName="'Home'"
 >
     @if(session()->has('success'))
-{{--        {{ session()->forget('success') }}--}}
         <div class="absolute top-5 right-10 w-[25rem]">
-            <div x-data="toastNotification()" x-init="openToast()" x-show="open"
-                 x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 transform scale-90"
-                 x-transition:enter-end="opacity-100 transform scale-100" x-transition:leave="transition ease-in duration-300"
-                 x-transition:leave-start="opacity-100 transform scale-100"
-                 x-transition:leave-end="opacity-0 transform scale-90">
-                <div class="p-4 rounded-md relative" :class="{'bg-green-500 text-white': success, 'bg-red-500 text-white': !success}">
-                    <button @click="open = false" class="absolute top-0 right-0 m-2 text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                        </svg>
-                    </button>
-                    <h4 class="font-bold" x-text="title"></h4>
-                    <p x-text="message"></p>
-                </div>
-            </div>
+            <x-alpine.alert />
         </div>
     @endif
     <div class="grid place-items-center p-7">
