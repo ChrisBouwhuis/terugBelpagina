fd0b3ef5e163c0ce25e677aca85faec02e6275fd
Chris Bouwhuis <chrisb190203@gmail.com>
2024-05-30 17:14:41 +0200

    heb een roode border togevoegd wanneer de input fout is

--- resources/views/components/input-fields/text-area.blade.php
+++ resources/views/components/input-fields/text-area.blade.php
@@ -9,6 +9,7 @@
             class="p-3 fa-{{ $iconType }} fa-{{ $icon }}"></i></div>
     <textarea {{ $attributes }}
               class="w-full p-3 border border-gray-300 rounded-r-md shadow-sm bg-slate-200
-                   dark:placeholder-black/60 focus:ring-[#DCE0D9] focus:border-[#DCE0D9] sm:text-sm min-h-max"
+                   dark:placeholder-black/60 focus:ring-[#DCE0D9] focus:border-[#DCE0D9] sm:text-sm min-h-max
+                   {{ $errors->has($attributes['wire:model.live']) ? 'border-red-500 border-2' : 'border-gray-300' }}"
               placeholder="{{ $placeholder }}"></textarea>
 </div>
--- resources/views/components/input-fields/text-line.blade.php
+++ resources/views/components/input-fields/text-line.blade.php
@@ -10,7 +10,7 @@
     <div class="bg-slate-400 flex justify-center items-center h-12"><i
             class="p-3 fa-{{ $iconType }} fa-{{ $icon }}"></i></div>
     <input type="text" {{ $attributes }}
-    class="w-full h-12 p-3 border border-gray-300 rounded-r-md shadow-sm bg-slate-200
+    class="w-full h-12 p-3 border border-gray-300 rounded-r-md shadow-sm bg-slate-200  {{ $errors->has($attributes['wire:model.live']) ? 'border-red-500 border-2' : 'border-gray-300' }}
                 dark:placeholder-black/60 focus:ring-[#DCE0D9] focus:border-[#DCE0D9] sm:text-sm"
            placeholder="{{ $placeholder }}">
 </div>
